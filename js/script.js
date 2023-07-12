75ec3d851f5c04423e3d6050150f9e1ac7c1841e
leychenkoMaxim <max242009@ukr.net>
2023-07-12 12:54:58 +0300

    play2

--- js/script.js
+++ js/script.js
@@ -38,61 +38,61 @@ class Play {
 }
 
 let imagePlay = [
-  "../img/Screenshot_1.jpg",
+  "/img/Screenshot_1.jpg",
 
-  "../img/Screenshot_2.jpg",
+  "/img/Screenshot_2.jpg",
 
-  "../img/Screenshot_3.jpg",
+  "/img/Screenshot_3.jpg",
 
-  "../img/Screenshot_4.jpg",
+  "/img/Screenshot_4.jpg",
 
-  "../img/Screenshot_5.jpg",
+  "/img/Screenshot_5.jpg",
 
-  "../img/Screenshot_6.jpg",
+  "/img/Screenshot_6.jpg",
 
-  "../img/Screenshot_7.jpg",
+  "/img/Screenshot_7.jpg",
 
-  "../img/Screenshot_8.jpg",
+  "/img/Screenshot_8.jpg",
 
-  "../img/Screenshot_9.jpg",
+  "/img/Screenshot_9.jpg",
 
-  "../img/Screenshot_10.jpg",
+  "/img/Screenshot_10.jpg",
 
-  "../img/Screenshot_11.jpg",
+  "/img/Screenshot_11.jpg",
 
-  "../img/Screenshot_12.jpg",
+  "/img/Screenshot_12.jpg",
 
-  "../img/Screenshot_13.jpg",
+  "/img/Screenshot_13.jpg",
 
-  "../img/Screenshot_14.jpg",
+  "/img/Screenshot_14.jpg",
 
-  "../img/Screenshot_15.jpg",
-  "../img/Screenshot_16.jpg",
+  "/img/Screenshot_15.jpg",
+  "/img/Screenshot_16.jpg",
 
-  "../img/Screenshot_17.jpg",
+  "/img/Screenshot_17.jpg",
 
-  "../img/Screenshot_18.jpg",
+  "/img/Screenshot_18.jpg",
 
-  "../img/Screenshot_19.jpg",
+  "/img/Screenshot_19.jpg",
 
-  "../img/Screenshot_20.jpg",
+  "/img/Screenshot_20.jpg",
 
-  "../img/Screenshot_21.jpg",
+  "/img/Screenshot_21.jpg",
 
-  "../img/Screenshot_22.jpg",
+  "/img/Screenshot_22.jpg",
 
-  "../img/Screenshot_23.jpg",
+  "/img/Screenshot_23.jpg",
 
-  "../img/Screenshot_24.jpg",
-  "../img/Screenshot_25.jpg",
+  "/img/Screenshot_24.jpg",
+  "/img/Screenshot_25.jpg",
 
-  "../img/Screenshot_26.jpg",
+  "/img/Screenshot_26.jpg",
 
-  "../img/Screenshot_27.jpg",
+  "/img/Screenshot_27.jpg",
 
-  "../img/Screenshot_28.jpg",
+  "/img/Screenshot_28.jpg",
 
-  "../img/Screenshot_29.jpg",
+  "/img/Screenshot_29.jpg",
 ]
 
 let r = new Play(imagePlay)
